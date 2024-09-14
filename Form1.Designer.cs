9391249f9f06deabc1f4113d7be702b7ee651f6d
maryan213 <64809930+maryan213@users.noreply.github.com>
2024-09-14 18:46:34 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace cnv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.metru1 = new System.Windows.Forms.ComboBox();
+            this.metru2 = new System.Windows.Forms.ComboBox();
+            this.unit = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(150, 161);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 45);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(479, 161);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 45);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Myanmar Text", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(356, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "CONVERT";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // metru1
+            // 
+            this.metru1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.metru1.FormattingEnabled = true;
+            this.metru1.Location = new System.Drawing.Point(150, 212);
+            this.metru1.Name = "metru1";
+            this.metru1.Size = new System.Drawing.Size(180, 24);
+            this.metru1.TabIndex = 3;
+            this.metru1.SelectedIndexChanged += new System.EventHandler(this.metru1_SelectedIndexChanged);
+            // 
+            // metru2
+            // 
+            this.metru2.FormattingEnabled = true;
+            this.metru2.Items.AddRange(new object[] {
+            "Millimeter (mm)",
+            "Centimeter (cm) ",
+            "Decimeter (dm) ",
+            "Decameter (dam) ",
+            "Hectometer (hm)",
+            "Kilometer (km)"});
+            this.metru2.Location = new System.Drawing.Point(479, 212);
+            this.metru2.Name = "metru2";
+            this.metru2.Size = new System.Drawing.Size(180, 24);
+            this.metru2.TabIndex = 4;
+            this.metru2.SelectedIndexChanged += new System.EventHandler(this.metru2_SelectedIndexChanged);
+            // 
+            // unit
+            // 
+            this.unit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.unit.FormattingEnabled = true;
+            this.unit.Items.AddRange(new object[] {
+            "Length",
+            "Weight",
+            "Volume"});
+            this.unit.Location = new System.Drawing.Point(362, 42);
+            this.unit.Name = "unit";
+            this.unit.Size = new System.Drawing.Size(100, 24);
+            this.unit.TabIndex = 5;
+            this.unit.SelectedIndexChanged += new System.EventHandler(this.unit_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Myanmar Text", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(357, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "SELECT UNIT";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.unit);
+            this.Controls.Add(this.metru2);
+            this.Controls.Add(this.metru1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox metru1;
+        private System.Windows.Forms.ComboBox metru2;
+        private System.Windows.Forms.ComboBox unit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cnv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BackColor = Color.FromArgb(130, 197, 121);
+            button1.BackColor = Color.FromArgb(182, 221, 177);
+            button1.FlatAppearance.BorderSize = 0;
+            textBox1.BackColor = Color.FromArgb(234, 245, 232);
+            textBox2.BackColor = Color.FromArgb(234, 245, 232);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox2.BorderStyle = BorderStyle.None;
+            unit.BackColor = Color.FromArgb(182, 221, 177);
+            metru1.BackColor = Color.FromArgb(182, 221, 177);
+            metru2.BackColor = Color.FromArgb(182, 221, 177);
+            unit.FlatStyle = FlatStyle.Flat;
+            metru1.FlatStyle = FlatStyle.Flat;
+            metru2.FlatStyle = FlatStyle.Flat;
+        }
+
+       //-----------------GLOBALE-------------------//
+        string[] len = { "Millimeter (mm)", 
+                        "Centimeter (cm)", 
+                        "Decimeter (dm)", 
+                        "Meter (m)", 
+                        "Decameter (dam)",
+                        "Hectometer (hm)", 
+                        "Kilometer (km)" };
+
+        string[] wei = { "Milligram (mg)",
+                        "Centigram (cg)", 
+                        "Decigram (dg)",
+                        "Gram (g)", 
+                        "Decagram (dag)", 
+                        "Hectogram (hg)", 
+                        "Kilogram (kg)" };
+
+        string[] vol= { "Milliliter (mL)",
+                        "Centiliter (cL)", 
+                        "Deciliter (dL)", 
+                        "Liter (L)", 
+                        "Decaliter (daL)", 
+                        "Hectoliter (hL)",
+                        "Kiloliter (kL)"};
+
+
+        //-------------------------------------------//
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+            int p = 1;
+            if (unit.Text == "Length" || unit.Text == "Weight" || unit.Text == "Volume")
+            {
+                
+
+                if (metru1.SelectedIndex < metru2.SelectedIndex)
+                {
+                    int z = metru1.SelectedIndex;
+                    while (metru1.SelectedIndex < metru2.SelectedIndex) {
+                        p *= 10;
+                        metru1.SelectedIndex++;
+                    }
+                    metru1.Text = metru1.Items[z].ToString();
+                    float a = float.Parse(textBox1.Text);
+                    float b = a / p ;
+                    textBox2.Text = b.ToString();
+                }
+                else
+                {
+                    while (metru1.SelectedIndex > metru2.SelectedIndex)
+                    {
+                        p *= 10;
+                        metru1.SelectedIndex--;
+                    }
+                    float a = int.Parse(textBox1.Text);
+                    float b = a / p;
+                    textBox2.Text = b.ToString();
+                }
+
+            }
+
+            
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metru1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void metru2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void unit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (unit.Text == "Length")
+            {
+                metru1.Items.Clear();
+                metru2.Items.Clear();
+                metru1.Text = null;
+                metru2.Text = null;
+                for (int i = 0; i< len.Length; i++)
+                {
+                    metru1.Items.Add(len[i]);
+                    metru2.Items.Add(len[i]);
+                }
+            }
+            if (unit.Text == "Weight")
+            {
+                metru1.Items.Clear();
+                metru2.Items.Clear();
+                metru1.Text = null;
+                metru2.Text = null;
+                for (int i = 0; i < wei.Length; i++)
+                {
+                    metru1.Items.Add(wei[i]);
+                    metru2.Items.Add(wei[i]);
+                }
+            }
+            if (unit.Text == "Volume")
+            {
+                metru1.Items.Clear();
+                metru2.Items.Clear();
+                metru1.Text = null;
+                metru2.Text = null;
+                for (int i = 0; i < vol.Length; i++)
+                {
+                    metru1.Items.Add(vol[i]);
+                    metru2.Items.Add(vol[i]);
+                }
+            }
+        }
+    }
+}
